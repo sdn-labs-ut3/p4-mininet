43a5c8c02a61ab5eda79fb54c80de4a22e34f563
Emmanuel Lavinal <emmanuel.lavinal@univ-tlse3.fr>
2024-10-17 16:46:08 +0200

    Updates to P4 tools for 2024-Sep-01

--- p4runtime_lib/helper.py
+++ p4runtime_lib/helper.py
@@ -26,7 +26,8 @@ class P4InfoHelper(object):
         p4info = p4info_pb2.P4Info()
         # Load the p4info file into a skeleton P4Info object
         with open(p4_info_filepath) as p4info_f:
-            google.protobuf.text_format.Merge(p4info_f.read(), p4info)
+            google.protobuf.text_format.Merge(p4info_f.read(), p4info,
+                                              allow_unknown_field=True)
         self.p4info = p4info
 
     def get(self, entity_type, name=None, id=None):
@@ -59,14 +60,14 @@ class P4InfoHelper(object):
     def __getattr__(self, attr):
         # Synthesize convenience functions for name to id lookups for top-level entities
         # e.g. get_tables_id(name_string) or get_actions_id(name_string)
-        m = re.search("^get_(\w+)_id$", attr)
+        m = re.search(r"^get_(\w+)_id$", attr)
         if m:
             primitive = m.group(1)
             return lambda name: self.get_id(primitive, name)
 
         # Synthesize convenience functions for id to name lookups
         # e.g. get_tables_name(id) or get_actions_name(id)
-        m = re.search("^get_(\w+)_name$", attr)
+        m = re.search(r"^get_(\w+)_name$", attr)
         if m:
             primitive = m.group(1)
             return lambda id: self.get_name(primitive, id)
